38974dabcc205a1705f468b05514eb7d12ee5f1b
umeshbsa <umesh.bsa@gmail.com>
2018-04-21 20:36:56 +0530

    local change

---
+++ app/src/main/java/com/app/architecture/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.architecture.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.architecture.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
